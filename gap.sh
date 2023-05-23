a56864f00d6a204006f93a5124c66c15ed0ec270
bogfoot <bogfoot94@yahoo.com>
2023-05-23 19:48:41 +0200

    Updated

--- gap.sh
+++ gap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git add .
 git commit -m "Updated"
-echo {$1}
-# git push -f https://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@github.com/Bogfoot/{$1}.git
+git push -f https://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@github.com/Bogfoot/$1.git
