122f0a605c03ffdc1ea8298e219827e82c8651a2
bogfoot <bogfoot94@yahoo.com>
2023-06-13 20:04:43 +0200

    Updated

--- potrosnja.py
+++ potrosnja.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import pandas as pd
 import numpy as np
-import datetime
 from tabulate import tabulate
 from currency_converter import CurrencyConverter
 import plotly.graph_objects as go
@@ -30,7 +29,8 @@ def newPriceCalculation(df, brojProizvoda=0):
         ukupno = -df["Cijena"][item] * df["Kolicina"][item]
         uk.append(ukupno)
         sum += ukupno
-        brojProizvoda += df["Kolicina"][item]
+        if df["Svrha"][item] != "plaća":
+            brojProizvoda += df["Kolicina"][item]
         cum_sum.append(sum)
     return cum_sum, brojProizvoda, uk
 
@@ -41,24 +41,13 @@ def newDate(df):
 
 def showPlots(df):
     fig = go.Figure()
-    imName = (
-        "Slike/GrafPotrošnje" + datetime.datetime.today().strftime("%d_%m_%Y") + ".png"
-    )
+    # imName = ( "Slike/GrafPotrošnje" + datetime.datetime.today().strftime("%d_%m_%Y") + ".png")
     fig.add_trace(go.Line(x=df["Datum"], y=df["Cijena"], name="Potršnja"))
     fig.add_trace(
         go.Line(x=df["Datum"], y=df["Kumulativna suma"], name="Kumulativna potrošnja")
     )
-
-    # fig.add_trace(
-    #     go.Line(
-    #         x=df["Datum"],
-    #         y=line_fit(np.linspace(0, len(df["Datum"]), len(df["Datum"])), k, l),
-    #         name="Linear fit",
-    #         mode="markers",
-    #     )
-    # )
-    # pio.write_image(fig, imName, width=2000, height=2000)
     return fig.show()
+
 
 
 def showStatistics(df, brProiz):
@@ -66,7 +55,6 @@ def showStatistics(df, brProiz):
             Srednja vrijednost proizvoda: {round(float(df['Cijena'].mean()),2)} {df['Valuta'][df['Cijena'].idxmax()]} \n\
             Standardna devijacija: {round(float(df['Cijena'].std()),2)} {df['Valuta'][df['Cijena'].idxmax()]}\n\
             Medijan: {round(float(df['Cijena'].median()),2)} {df['Valuta'][df['Cijena'].idxmax()]}\n\
-            Potrošnja bez najskupljeg proizvoda: {round(float(df['Kumulativna suma'].iloc[-1] - df['Cijena'].max()),2)} {df['Valuta'][df['Cijena'].idxmax()]}\n\
             Srednja vrijednost: {round(float((df['Kumulativna suma'].iloc[-1])/brProiz),2)} {df['Valuta'][df['Cijena'].idxmax()]}\n\
             Današnja potrošnja: {round(float(df['Dnevna Potrošnja'].iloc[-1]),2)} {df['Valuta'][df['Cijena'].idxmax()]}"
 
@@ -88,8 +76,6 @@ if not os.path.exists("Slike"):
 df["Datum"] = newDate(df)
 df["Datum"] = df['Datum'].dt.date
 length = len(df["Cijena"])
-# td = pd.Series([pd.Timedelta(milliseconds=i) for i in range(length)])
-# df["Datum"] += td
 df["Kumulativna suma"], brProiz, df["Ukupna cijena"] = newPriceCalculation(df)
 df.sort_values(by="Datum")
 
